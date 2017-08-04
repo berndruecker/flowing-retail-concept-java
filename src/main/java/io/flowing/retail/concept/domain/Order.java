57cf9ca57b1904a3383f9685613d50a3bede10d0
Bernd Rücker <bernd.ruecker@camunda.com>
2017-08-04 11:24:05 +0200

    added simple empty OrderEntity

--- src/main/java/io/flowing/retail/concept/domain/Order.java
+++ src/main/java/io/flowing/retail/concept/domain/Order.java
@@ -9,28 +9,13 @@ public class Order implements BusObserver {
   public static void init() {
     Bus.register(new Order());
   }
-
-  /**
-   * Reasons for state handling
-   * - Monitoring
-   * - Timeout / Escalation
-   * - Parallel Processing / Merging
-   * - Auditing
-   */
   
   public void eventReceived(Event event) {
     if (event.is("OrderPlaced")) {
-      Bus.send(new Event("RetrievePaymentCommand", event.getPayload()));
-      // now we need to persist some data, as we do not send everything along to payment
-      
-      // we might also want to watch the latest status / check for timeout. What if payment does not return?
-      
-      // you definitely need to keep track of the state if you have loops (?)
+      Bus.send(new Event("RetrievePaymentCommand", event.getPayload())); 
     }
     if (event.is("PaymentReceived")) {
-      Bus.send(new Event("FetchGoodsCommand", event.getPayload()));
-      
-      // we need to wait for failure messages to trigger compensation of payment
+      Bus.send(new Event("FetchGoodsCommand", event.getPayload()));      
     }
     if (event.is("GoodsFetched")) {
       Bus.send(new Event("ShipGoodsCommand", event.getPayload()));
---
+++ src/main/java/io/flowing/retail/concept/domain/OrderEntity.java
@@ -0,0 +1,10 @@
+package io.flowing.retail.concept.domain;
+
+@SuppressWarnings("unused")
+public class OrderEntity {
+  
+  private String id;
+
+  private String orderContent; 
+
+}
