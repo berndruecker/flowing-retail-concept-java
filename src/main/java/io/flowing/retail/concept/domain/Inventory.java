6d070bc20334c5264dc4f7c5b85242413af40f67
Bernd Rücker <bernd.ruecker@camunda.com>
2017-08-22 10:49:43 +0200

    removed commented code

--- src/main/java/io/flowing/retail/concept/domain/Inventory.java
+++ src/main/java/io/flowing/retail/concept/domain/Inventory.java
@@ -15,19 +15,7 @@ public class Inventory implements EventObserver {
   public void eventReceived(Event event) {
     if (event.is("PaymentReceived")) {
       fetchGoods(event.getPayload());
-    }
-    
-//    if (event.is("PaymentReceived") && !(Boolean) event.getPayload().get("vip")) {
-//      fetchGoods(event.getPayload());
-//    }
-//    if (event.is("OrderPlaced") && (Boolean) event.getPayload().get("vip")) {
-//      fetchGoods(event.getPayload());
-//    }
-
-    // if (event.is("FetchGoodsCommand")) {
-    //  fetchGoods(event.getPayload());
-    // }
-
+    }   
   }
 
   public void fetchGoods(Map<String, Object> payload) {
